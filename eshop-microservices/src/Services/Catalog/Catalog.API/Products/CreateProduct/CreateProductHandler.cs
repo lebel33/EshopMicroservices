bff2a7c523889786479b08f697c47098502058d0
Lander Rubio J <lrubioj_22@hotmail.com>
2024-10-19 09:50:29 -0600

    cambios casa

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
 
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
@@ -10,7 +9,6 @@ namespace Catalog.API.Products.CreateProduct
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-
             var product = new Product
             {
                 Name = command.Name,
